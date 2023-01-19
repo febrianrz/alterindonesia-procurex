3b4aa67a284cb16ebf48919b4c0e0ef39464c91a
Developer <developer@developer.com>
2023-01-19 10:40:46 +0700

    Add test unit

---
+++ tests/Unit/TestUnit.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Tests\Unit;
+
+use PHPUnit\Framework\TestCase;
+
+class TestUnit extends TestCase
+{
+    /**
+     * A basic unit test example.
+     *
+     * @return void
+     */
+    public function test_example()
+    {
+        $this->assertTrue(true);
+    }
+}
