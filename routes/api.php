79f1a556f6b44b288655d16de19ae10aaf326570
febrian reza <febrianrz@gmail.com>
2023-01-26 03:28:54 +0000

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -2,8 +2,8 @@
 
 use Illuminate\Support\Facades\Route;
 
-const ID = "/{id}";
-const RESTORE_PATH = "/restore/{id}";
+//const ID = "/{id}";
+//const RESTORE_PATH = "/restore/{id}";
 
 Route::middleware(['log.activity'])->group(function () {
     Route::get('/ping', function () {
@@ -18,13 +18,13 @@ Route::middleware(['log.activity'])->group(function () {
         Route::post('/logout', 'App\Http\Controllers\LoginController@logout')->name('api.logout');
 
         # User Management
-        require __DIR__."/UserManagement/role-api.php";
-        require __DIR__."/UserManagement/permission-api.php";
+    //    require __DIR__."/UserManagement/role-api.php";
+    //    require __DIR__."/UserManagement/permission-api.php";
 
         # Module Management
-        require __DIR__."/ModuleManagement/module-api.php";
-        require __DIR__."/ModuleManagement/menu-api.php";
-        require __DIR__."/ModuleManagement/sub-menu-api.php";
+    //    require __DIR__."/ModuleManagement/module-api.php";
+    //    require __DIR__."/ModuleManagement/menu-api.php";
+    //    require __DIR__."/ModuleManagement/sub-menu-api.php";
     });
 });
 
