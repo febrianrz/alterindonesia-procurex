0c8ef1d598f88557fb9967fb4210ba13d510749b
Developer <developer@developer.com>
2023-01-30 12:37:59 +0700

    Update resource response helper

--- app/Traits/HasActionTrait.php
+++ app/Traits/HasActionTrait.php
@@ -5,11 +5,22 @@ namespace App\Traits;
 use Illuminate\Support\Facades\Route;
 
 trait HasActionTrait {
+
+    public function getIsCanEdit(): bool
+    {
+        return true;
+    }
+
+    public function getIsCanDelete(): bool
+    {
+        return true;
+    }
+
    public function getActions() {
        $mainRoute = Route::currentRouteName();
        return [
-          'update'      => route($mainRoute)."/".$this->id,
-          'delete'      => route($mainRoute)."/".$this->id,
+          'update'      => $this->getIsCanEdit() ? route($mainRoute)."/".$this->id : null,
+          'delete'      => $this->getIsCanDelete() ? route($mainRoute)."/".$this->id : null,
        ];
    }
 }
