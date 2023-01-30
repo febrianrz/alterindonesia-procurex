2f72fa3178f6e58d6f5561b2ae3419f711c5d6ef
Developer <developer@developer.com>
2023-01-30 12:09:53 +0700

    Fixing bugs defined constant

--- routes/api.php
+++ routes/api.php
@@ -2,8 +2,14 @@
 
 use Illuminate\Support\Facades\Route;
 
-//const ID = "/{id}";
-//const RESTORE_PATH = "/restore/{id}";
+if(!defined('ID')){
+    define("ID", "/{id}");
+}
+if(!defined('RESTORE_PATH')){
+    define("RESTORE_PATH", "/restore/{id}");
+}
+
+
 
 Route::middleware(['log.activity'])->group(function () {
     Route::get('/ping', function () {
@@ -18,13 +24,13 @@ Route::middleware(['log.activity'])->group(function () {
         Route::post('/logout', 'App\Http\Controllers\LoginController@logout')->name('api.logout');
 
         # User Management
-//        require __DIR__."/UserManagement/role-api.php";
-//        require __DIR__."/UserManagement/permission-api.php";
-//
-//        # Module Management
-//        require __DIR__."/ModuleManagement/module-api.php";
-//        require __DIR__."/ModuleManagement/menu-api.php";
-//        require __DIR__."/ModuleManagement/sub-menu-api.php";
+        require __DIR__."/UserManagement/role-api.php";
+        require __DIR__."/UserManagement/permission-api.php";
+
+        # Module Management
+        require __DIR__."/ModuleManagement/module-api.php";
+        require __DIR__."/ModuleManagement/menu-api.php";
+        require __DIR__."/ModuleManagement/sub-menu-api.php";
     });
 });
 
