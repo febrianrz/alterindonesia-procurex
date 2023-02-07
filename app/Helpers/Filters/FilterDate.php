ad0f2cb0076a3f69102295a7292e8d314e4b581a
Kholis Abdullah <kholis.abdullah@gmail.com>
2023-02-07 11:11:51 +0700

    feat: add filter, sort, and relationships

---
+++ app/Helpers/Filters/FilterDate.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Helpers\Filters;
+
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Support\Carbon;
+use Spatie\QueryBuilder\Filters\Filter;
+
+class FilterDate implements Filter
+{
+    public function __invoke(Builder $query, $value, string $property)
+    {
+        if (! is_array($value)) {
+            return;
+        }
+
+        if (count($value) < 2) {
+            return;
+        }
+
+        $query->where(fn (Builder $query) => $query
+            ->where('created_at', '>=', Carbon::parse($value[0])->startOfDay())
+            ->where('created_at', '<=', Carbon::parse($value[1])->endOfDay())
+        );
+    }
+}
