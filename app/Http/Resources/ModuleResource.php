4af22b918b22547baddbb3218e4c25cf4e5aeff9
Developer <developer@developer.com>
2023-02-08 11:37:58 +0700

    Update searchable mechanism service

--- app/Http/Resources/ModuleResource.php
+++ app/Http/Resources/ModuleResource.php
@@ -20,17 +20,7 @@ class ModuleResource extends JsonResource
     {
         return [
             ...$this->resource->makeHidden('deleted_at')->toArray(),
-
-            // "id"                    => $this->whenHas('id'),
-            // "name"                  => $this->whenHas('name'),
-            // "icon"                  => $this->whenHas('icon'),
-            // "status"                => $this->whenHas('status'),
-            // "path"                  => $this->whenHas('path'),
-            // "order_no"              => $this->whenHas('order_no'),
-            // "is_show_on_dashboard"  => $this->whenHas('is_show_on_dashboard'),
-
             "menus"     => MenuResource::collection($this->whenLoaded('menus')),
-
             "action"    => $this->whenHas('id', fn () => [
                 "edit"  => Auth::user()->can("update") ? route('api.module.update', $this->id) : null,
                 "delete"=> Auth::user()->can("destroy") ? route('api.module.destroy', $this->id) : null,
