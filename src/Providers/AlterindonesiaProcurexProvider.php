640b987fb3954031caae27c8486fead32f3b70d5
Developer <developer@developer.com>
2023-02-16 15:31:42 +0700

    Add routes name list

--- src/Providers/AlterindonesiaProcurexProvider.php
+++ src/Providers/AlterindonesiaProcurexProvider.php
@@ -27,5 +27,6 @@ class AlterindonesiaProcurexProvider extends ServiceProvider
             __DIR__.'/../config/procurex.php' => config_path('procurex.php'),
         ]);
         $this->loadRoutesFrom(__DIR__.'/../routes/web.php');
+        $this->loadMigrationsFrom(__DIR__.'/../database/migrations');
     }
 }
--- src/routes/web.php
+++ src/routes/web.php
@@ -14,5 +14,18 @@ Route::prefix('/api',function(){
             'datetime'   => \Carbon\Carbon::now()->format("d F Y, H:i:s")
         ]);
     });
+
+    // List all routes
+    Route::get('/routes',function(){
+        $routeCollection = Illuminate\Support\Facades\Route::getRoutes();
+        $routes = [];
+        foreach ($routeCollection as $value) {
+            if (str_starts_with($value->getName(), 'api.')){
+                $routes[] = $value->getName();
+            }
+        }
+        return \Alterindonesia\Procurex\Facades\GlobalHelper::responseSuccess("Success",$routes);
+
+    });
 });
 
