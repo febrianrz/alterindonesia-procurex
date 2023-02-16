83a2c07eb4c482dcadfd17105fd5a6f3b61d318c
Developer <developer@developer.com>
2023-02-16 15:37:33 +0700

    Fixing bugs prefix /api

--- src/routes/web.php
+++ src/routes/web.php
@@ -2,7 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::prefix('/api',function(){
+Route::group(['prefix' => '/api'], function(){
     Route::get('/ping', function () {
         return response()->json([
             'message'   => 'ok'
