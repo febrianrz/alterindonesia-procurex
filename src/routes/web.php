93366d5384a0722673eac151cd393422ea02f0b1
Developer <developer@developer.com>
2023-02-23 13:10:09 +0700

    Add route assign name

--- src/routes/web.php
+++ src/routes/web.php
@@ -19,7 +19,7 @@ Route::group(['prefix' => '/api'], function(){
         Route::middleware(['auth.jwt'])->group(function () {
             // List all routes
             Route::get('/routes','\Alterindonesia\Procurex\Controllers\AlterindonesiaProcurexController@getRouteList');
-            Route::post('/routes/assign','\Alterindonesia\Procurex\Controllers\AlterindonesiaProcurexController@assignRoleRoute');
+            Route::post('/routes/assign','\Alterindonesia\Procurex\Controllers\AlterindonesiaProcurexController@assignRoleRoute')->name('api.route.assign');
 
         });
     });
