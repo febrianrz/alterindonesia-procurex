235d5c5d229f0e0e3b9f1c6cfc5fe5598ee3046d
Developer <developer@developer.com>
2023-02-23 11:41:29 +0700

    Add state menu service route

--- app/Http/Resources/MenuServiceRouteResource.php
+++ app/Http/Resources/MenuServiceRouteResource.php
@@ -2,8 +2,10 @@
 
 namespace App\Http\Resources;
 
+use App\Models\Role;
 use App\Models\Service;
 use Illuminate\Http\Resources\Json\JsonResource;
+use Illuminate\Support\Facades\DB;
 
 class MenuServiceRouteResource extends JsonResource
 {
@@ -16,11 +18,19 @@ class MenuServiceRouteResource extends JsonResource
     public function toArray($request)
     {
         $service = Service::find($this->pivot->service_id);
+        $state = false;
+        if($request->has('role_code') && $request->input('role_code')){
+            $state = (bool)DB::table('role_permission_procurex')
+                ->where('role_code', $request->input('role_code'))
+                ->where('permission_name', $this->pivot->route)
+                ->first();
+        }
         return [
             'service_id'    => $this->pivot->service_id,
             'route'         => $this->pivot->route,
             'service_name'  => $service->name,
-            'url'           => $service->url
+            'url'           => $service->url,
+            'state'         => $state
         ];
     }
 }
