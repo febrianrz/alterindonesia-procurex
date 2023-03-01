7bd4ea0ef4952b61f47744c86a50367e93f7dbd9
Developer <developer@developer.com>
2023-03-01 12:55:15 +0700

    Add resource format

---
+++ src/Traits/WithMetaTagResource.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Alterindonesia\Procurex\Traits;
+
+use App\Libraries\Auth;
+use Illuminate\Support\Facades\Route;
+
+trait WithMetaTagResource
+{
+    protected int $code = 200;
+
+    public function with($request): array
+    {
+        return [
+            'meta' => [
+                'message' => 'Success',
+                'code' => $this->code,
+                'create'    => $this->canCreate()
+            ],
+        ];
+    }
+
+    public function withCode(int $code): static
+    {
+        $this->code = $code;
+
+        return $this;
+    }
+
+    public function canCreate(): bool
+    {
+        return true;
+    }
+}
