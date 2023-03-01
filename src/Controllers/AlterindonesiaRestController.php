608771d17d90980098e73fceda6a0e80ed69578e
Developer <developer@developer.com>
2023-03-01 13:09:26 +0700

    Add rest controller

---
+++ src/Controllers/AlterindonesiaRestController.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Alterindonesia\Procurex\Facades\Auth;
+use Alterindonesia\Procurex\Facades\GlobalHelper;
+use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
+use Illuminate\Foundation\Bus\DispatchesJobs;
+use Illuminate\Foundation\Validation\ValidatesRequests;
+use Illuminate\Routing\Controller as BaseController;
+
+class AlterindonesiaRestController extends BaseController
+{
+    use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+
+    public function responseSuccess($message, $data=[], $code=200, $resource=null)
+    {
+        return GlobalHelper::responseSuccess($message, $data, $code, $resource);
+    }
+
+    public function responseError($message, $data=[], $code=400)
+    {
+        return GlobalHelper::responseError($message, $data, $code);
+    }
+
+    public function can($permissionName) : bool
+    {
+        return Auth::user()->hasPermission($permissionName);
+    }
+
+    public function canWithThrow($permissionName)
+    {
+        if (!$this->can($permissionName)) {
+            throw new \Exception("Forbidden",403);
+        }
+    }
+}
