aa531411ba5cf54ebffebea8c5b8a1a927f876a1
Developer <developer@developer.com>
2023-03-01 13:21:47 +0700

    Add controller and services interface

--- src/Controllers/AlterindonesiaRestController.php
+++ src/Controllers/AlterindonesiaRestController.php
@@ -8,7 +8,7 @@ use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 use Symfony\Component\HttpFoundation\Response;
 
-class MasterDataController extends AlterindonesiaBaseController
+class AlterindonesiaRestController extends AlterindonesiaBaseController
 {
     /**
      * @var RestServiceInterface
