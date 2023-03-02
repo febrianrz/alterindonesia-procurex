8f6ac374ccfbe0ffa53ec976ea58e09440eb74f8
Developer <developer@developer.com>
2023-03-02 13:16:52 +0700

    Add action resource

--- src/Traits/HasActionTrait.php
+++ src/Traits/HasActionTrait.php
@@ -3,6 +3,7 @@
 namespace Alterindonesia\Procurex\Traits;
 
 use Alterindonesia\Procurex\Facades\Auth;
+use Illuminate\Http\Request;
 
 trait HasActionTrait {
     public static function canStore():bool
@@ -24,5 +25,30 @@ trait HasActionTrait {
         $routeName = request()->route()->getName();
         $routeName = str_replace(['.index','.update','.destroy'],[".destroy"],$routeName);
         return Auth::user()->can($routeName);
+    }
+
+    public function action(Request $request){
+        return $this->whenHas(
+            'id',
+            function () use ($request) {
+                // set action
+                $action = [
+                    "edit"  => self::canUpdate(),
+                    "delete"=> self::canDestroy(),
+                    "restore"  => self::canDestroy()
+                ];
+
+                // check if trashed resource
+                if ($request->has("filter")
+                    && array_key_exists("trashed", $request->input('filter'))
+                ) {
+                    unset($action["delete"]);
+                } else {
+                    unset($action["restore"]);
+                }
+
+                return $action;
+            }
+        );
     }
 }
