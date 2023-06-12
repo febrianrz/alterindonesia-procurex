6abcc05094beeccef91be0ca7408871f8c58f710
Developer <developer@developer.com>
2023-06-12 14:58:52 +0700

    update endpoint file to api/file

--- src/routes/web.php
+++ src/routes/web.php
@@ -25,6 +25,6 @@ Route::group(['prefix' => '/api'], function(){
         });
     });
 
-    Route::get('/file','\Alterindonesia\Procurex\Controllers\AlterindonesiaProcurexController@getFile');
+    Route::get('/api/file','\Alterindonesia\Procurex\Controllers\AlterindonesiaProcurexController@getFile');
 });
 
