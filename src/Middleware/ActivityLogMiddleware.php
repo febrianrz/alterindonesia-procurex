80d4f9884322d7adf38680016b256601f7a2ed32
Developer <developer@developer.com>
2023-08-08 11:38:44 +0700

    Update: notification discord add filename and line no

--- src/Middleware/ActivityLogMiddleware.php
+++ src/Middleware/ActivityLogMiddleware.php
@@ -34,12 +34,20 @@ class ActivityLogMiddleware
                 $table->longText('exception')->nullable();
             });
         }
+        if(!\Schema::hasColumn('user_logs','http_code')){
+            \Schema::table('user_logs',function (Blueprint $table){
+                $table->longText('http_code')->nullable();
+            });
+        }
         $fullUrl = url()->full();
         $uri = $request->path();
         $routeName = Route::currentRouteName();
         $routeAction = Route::currentRouteAction();
         $payload = $request->all();
         $method = $request->method();
+        $file = $response->exception->getFile();
+        $line = $response->exception->getLine();
+
         $response = (array)json_decode($response->getContent());
         $user = $this->getUser($request);
         $logId = UserLog::create([
@@ -59,14 +67,14 @@ class ActivityLogMiddleware
             'agent'         => $request->userAgent(),
             'ip'            => $request->ip()
         ]);
-        if(config('procurex.is_send_error_to_discord',false) && $exception){
+        if(config('procurex.is_send_error_to_discord',false) && $exception && $response->getStatusCode() !== 422){
             try {
                 $hookUrl = "https://discord.com/api/webhooks/1137646975685234749/bg2jVge6T-3DLJ2_bHMJikEmOr3N6otXY9XApUNHZEecmc8gUCMp6UywwKipEqmNkwM8";
-                $serviceName = config('procurex.service_name', 'Procurex');
+                $serviceName = config('procurex.service_name', 'Procurex')." ".config('app.env');
                 $http = \Http::withHeaders([
                     'Content-Type' => 'application/json'
                 ])->timeout(3)->post($hookUrl, [
-                    'content' => "Error Handler {$serviceName} ".(env('APP_ENV') ? 'Development' : 'Production')." with Log ID: {$logId->id}, Error: {$exception->getMessage()}"
+                    'content' => "Service {$serviceName} \nUser LogID: $logId->id \nFile: $file \nLine: $line \nError: {$exception->getMessage()} "
                 ]);
             } catch (\Exception $e) {
             }
