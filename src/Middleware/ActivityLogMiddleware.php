3edd4d16afa9f153fe38a66fa3f650f3115fa57f
Developer <developer@developer.com>
2023-08-09 10:29:37 +0700

    Fix: try to get exception on log

--- src/Middleware/ActivityLogMiddleware.php
+++ src/Middleware/ActivityLogMiddleware.php
@@ -28,7 +28,7 @@ class ActivityLogMiddleware
 
     private function writeLog(Request $request, $response): void
     {
-        $exception = $response->exception;
+        $exception = method_exists($response->exception) ? $response->exception : null;
         if(!\Schema::hasColumn('user_logs','exception')){
             \Schema::table('user_logs',function (Blueprint $table){
                 $table->longText('exception')->nullable();
@@ -46,8 +46,8 @@ class ActivityLogMiddleware
         $payload = $request->all();
         $method = $request->method();
 
-        $file = $response->exception ? $response->exception->getFile() : '';
-        $line = $response->exception ? $response->exception->getLine() : '';
+        $file = $exception? $exception->getFile() : '';
+        $line = $exception ? $exception->getLine() : '';
 
         $response = (array)json_decode($response->getContent());
         $user = $this->getUser($request);
@@ -72,10 +72,11 @@ class ActivityLogMiddleware
             try {
                 $hookUrl = "https://discord.com/api/webhooks/1137646975685234749/bg2jVge6T-3DLJ2_bHMJikEmOr3N6otXY9XApUNHZEecmc8gUCMp6UywwKipEqmNkwM8";
                 $serviceName = config('procurex.service_name', 'Procurex')." ".config('app.env');
+                $message = $exception ? $exception->getMessage() : '';
                 $http = \Http::withHeaders([
                     'Content-Type' => 'application/json'
                 ])->timeout(3)->post($hookUrl, [
-                    'content' => "Service {$serviceName} \nUser LogID: $logId->id \nFile: $file \nLine: $line \nError: {$exception->getMessage()} "
+                    'content' => "Service {$serviceName} \nUser LogID: $logId->id \nFile: $file \nLine: $line \nError: {$message} "
                 ]);
             } catch (\Exception $e) {
             }
