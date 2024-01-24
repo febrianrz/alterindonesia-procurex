7419579426603a328e0979f05e8f82d4cb109a82
Kholis Abdullah <kholis.abdullah@gmail.com>
2024-01-25 06:53:06 +0700

    fix: restore send to discord in activity log middleware

--- src/Middleware/ActivityLogMiddleware.php
+++ src/Middleware/ActivityLogMiddleware.php
@@ -3,9 +3,14 @@ namespace Alterindonesia\Procurex\Middleware;
 
 use Alterindonesia\Procurex\Facades\Auth;
 use Closure;
+use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\App;
+use Illuminate\Support\Facades\Http;
 use Illuminate\Support\Facades\Route;
 use Alterindonesia\Procurex\Models\UserLog;
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Validation\ValidationException;
 
 
 class ActivityLogMiddleware
@@ -25,7 +30,14 @@ class ActivityLogMiddleware
         return $response;
     }
 
-    private function writeLog(Request $request, $response) {
+    private function writeLog(Request $request, $response): void
+    {
+        $exception = $response->exception;
+        if(!Schema::hasColumn('user_logs','exception')){
+            Schema::table('user_logs',function (Blueprint $table){
+                $table->longText('exception')->nullable();
+            });
+        }
         $fullUrl = url()->full();
         $uri = $request->path();
         $routeName = Route::currentRouteName();
@@ -34,7 +46,7 @@ class ActivityLogMiddleware
         $method = $request->method();
         $response = (array)json_decode($response->getContent());
         $user = $this->getUser($request);
-        UserLog::create([
+        $logId = UserLog::create([
             'user_id'       => $user->id,
             'username'      => $user->username,
             'email'         => $user->email,
@@ -47,10 +59,28 @@ class ActivityLogMiddleware
             'route_action'  => $routeAction,
             'payload'       => $payload,
             'response'      => $response,
+            'exception'     => $exception ?? '',
             'agent'         => $request->userAgent(),
             'ip'            => $request->ip()
         ]);
+        if(config('procurex.is_send_error_to_discord',false) && $exception){
+            if ($exception instanceof ValidationException) {
+                return;
+            }
+
+            try {
+                $hookUrl = "https://discord.com/api/webhooks/1137646975685234749/bg2jVge6T-3DLJ2_bHMJikEmOr3N6otXY9XApUNHZEecmc8gUCMp6UywwKipEqmNkwM8";
+                $serviceName = config('procurex.service_name', 'Procurex');
+                $http = Http::withHeaders([
+                    'Content-Type' => 'application/json'
+                ])->timeout(3)->post($hookUrl, [
+                    'content' => "Error Handler {$serviceName} ".(App::isProduction() ? 'Production' : 'Development')." with Log ID: {$logId->id}, Error: {$exception->getMessage()}"
+                ]);
+            } catch (\Exception $e) {
+            }
+        }
     }
+
 
     private function getUser(Request $request): object
     {
