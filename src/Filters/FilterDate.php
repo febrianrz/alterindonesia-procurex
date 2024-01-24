00058089b65f89a4353c17f3682c2c0295c614e2
Kholis Abdullah <kholis.abdullah@gmail.com>
2024-01-25 06:41:55 +0700

    fix: custom filter date for Spatie query builder

--- src/Filters/FilterDate.php
+++ src/Filters/FilterDate.php
@@ -4,23 +4,39 @@ namespace Alterindonesia\Procurex\Filters;
 
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Support\Carbon;
+use Illuminate\Support\Facades\Log;
 use Spatie\QueryBuilder\Filters\Filter;
 
 class FilterDate implements Filter
 {
-    public function __invoke(Builder $query, $value, string $property)
+    public function __invoke(Builder $query, $value, string $property): void
     {
-        if (! is_array($value)) {
+        if (! is_array($value) && ! is_string($value)) {
             return;
         }
 
-        if (count($value) < 2) {
+        if (is_array($value) && count($value) < 2) {
+            return;
+        }
+
+        if (is_string($value)) {
+            $startDate = $value;
+            $endDate = $value;
+        } else {
+            [$startDate, $endDate] = $value;
+        }
+
+        Log::info('Filter Date by [' . $startDate . '] - [' . $endDate.']');
+        $startDate = rescue(static fn () => Carbon::parse($startDate)->startOfDay(), report: false);
+        $endDate = rescue(static fn () => Carbon::parse($endDate)->endOfDay(), report: false);
+
+        if (! $startDate || ! $endDate) {
             return;
         }
 
         $query->where(fn (Builder $query) => $query
-            ->where('created_at', '>=', Carbon::parse($value[0])->startOfDay())
-            ->where('created_at', '<=', Carbon::parse($value[1])->endOfDay())
+            ->where($property, '>=', $startDate)
+            ->where($property, '<=', $endDate)
         );
     }
 }
